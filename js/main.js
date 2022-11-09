b019f7e550c95800f753c3bdb2dcb668f6beb4d6
Ruslan Symonenko <symonenkors@gmail.com>
2022-11-09 14:58:55 +0200

    made modal window for adding new tasks

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     const taskContainer = document.querySelector('.tasks__container');
     const addTask = document.querySelector('.add__task');
+    const modalTask = document.querySelector('.modal__task');
+    const appWrapper = document.querySelector('.wrapper');
+    const modalBtnClose = document.querySelector('.modal__btn-close');
 
-
-    addTask.addEventListener('click', () => {
+    function createNewTask () {
         let newTask = new Task('my task', '09.11.22', '13:42');
         newTask.render(taskContainer);
         console.log(newTask);
+    }
+
+    function showModal () {
+        appWrapper.classList.add('wrapper--inactive');
+        modalTask.classList.add('modal__task--active');
+    }
+
+    function closeModal () {
+        appWrapper.classList.remove('wrapper--inactive');
+        modalTask.classList.remove('modal__task--active');
+    }
+
+    addTask.addEventListener('click', () => {
+        showModal();
+    });
+
+    modalBtnClose.addEventListener('click', () => {
+        closeModal();
     })
 })
